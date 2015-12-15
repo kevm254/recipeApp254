6cab51827fe5291fd8431a220a6bd7bee8ea8a07
Kevin Miller <scotty_terrific@yahoo.com>
2015-12-15 04:19:34 -0600

    Added pagination and chefs listings

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'mini_magick', '3.8.0'
 gem 'font-awesome-rails'
 gem 'will_paginate', '3.0.7'
 gem 'bootstrap-will_paginate', '0.0.10'
+gem 'bcrypt', '3.1.7'
 
 group :development, :test do
   gem 'byebug'
---
+++ app/controllers/chefs_controller.rb
@@ -0,0 +1,44 @@
+class ChefsController < ApplicationController
+  def index
+    @chefs = Chef.paginate(page: params[:page], per_page: 3)
+  end
+
+  def new
+    @chef = Chef.new
+  end
+
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "Your account was successfully created"
+      redirect_to recipes_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "You have updated your profile successfully!"
+      redirect_to recipes_path
+    else
+      flash[:failure] = "Your profile did not update"
+      render 'edit'
+    end
+  end
+
+  def show
+    @chef = Chef.find(params[:id])
+    @recipes = @chef.recipes.paginate(page: params[:page], per_page: 3)
+  end
+
+  private
+  def chef_params
+    params.require(:chef).permit(:chefname, :email, :password)
+  end
+end
