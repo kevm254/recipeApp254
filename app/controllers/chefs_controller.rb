e2487652610d14aaea8d8da2921b7da2ba83d731
Kevin Miller <scotty_terrific@yahoo.com>
2015-12-16 06:36:19 -0600

    Fine tuned app

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,4 +1,7 @@
 class ChefsController < ApplicationController
+  before_action :set_chef, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
+
   def index
     @chefs = Chef.paginate(page: params[:page], per_page: 3)
   end
@@ -11,6 +14,7 @@ class ChefsController < ApplicationController
     @chef = Chef.new(chef_params)
     if @chef.save
       flash[:success] = "Your account was successfully created"
+      session[:chef_id] = @chef.id
       redirect_to recipes_path
     else
       render 'new'
@@ -18,14 +22,12 @@ class ChefsController < ApplicationController
   end
 
   def edit
-    @chef = Chef.find(params[:id])
   end
 
   def update
-    @chef = Chef.find(params[:id])
     if @chef.update(chef_params)
       flash[:success] = "You have updated your profile successfully!"
-      redirect_to recipes_path
+      redirect_to chef_path(@chef)
     else
       flash[:failure] = "Your profile did not update"
       render 'edit'
@@ -33,12 +35,22 @@ class ChefsController < ApplicationController
   end
 
   def show
-    @chef = Chef.find(params[:id])
     @recipes = @chef.recipes.paginate(page: params[:page], per_page: 3)
   end
 
   private
   def chef_params
     params.require(:chef).permit(:chefname, :email, :password)
+  end
+
+  def require_same_user
+    if current_user != @chef
+      flash[:danger] = 'You can only edit your own profile'
+      redirect_to root_path
+    end
+  end
+
+  def set_chef
+    @chef = Chef.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
   resources :chefs, except: [:new]
   get '/register', to: 'chefs#new'
 
+  #login and logout
+  get '/login', to: 'logins#new'
+  post '/login', to: 'logins#create'
+  get '/logout', to: 'logins#destroy'
 end
+
