a280c68a5a2bb5bc425de8a3a6f409489c21bc3b
Kevin Miller <scotty_terrific@yahoo.com>
2015-12-17 05:16:48 -0600

    Added styles and ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+chef = Chef.create(chefname: 'bob', email: 'bob@example.com', password: 'password')
